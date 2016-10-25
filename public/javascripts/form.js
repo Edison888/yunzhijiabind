fd91bfad87946c0753418dbb67f433830f4a26e5
YeasonHe <hmx197204@163.com>
2016-10-25 20:33:36 +0800

    动态页签

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -4,7 +4,8 @@ app.controller('form_detail', function ($scope, $http) {
     $http(
         {
             method: 'get',
-            url: requrl,
+            //url: requrl,
+            url: 'json/form',
             params: {
                 statuskey: statuskeyparam,
                 statuscode: statuscodeparam,
@@ -14,6 +15,12 @@ app.controller('form_detail', function ($scope, $http) {
         }
     ).success(function (response) {
             console.log(response)
+            $scope.tabcontent = response.data.taskbill.head.tabContent;
+            $('#myTab a').click(function (e) {
+                e.preventDefault()
+                $(this).tab('show')
+            });
+            $('#myTab a:first').tab('show');
         });
 
 });
