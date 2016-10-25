2b82d6b3fa8d1abb60674c580c6c2bf6389bb27c
YeasonHe <hmx197204@163.com>
2016-10-25 15:29:00 +0800

    表单详情初始化

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,11 @@ var router = express.Router();
 router.get('/binding', function(req, res, next) {
   res.render('binding');
 });
+router.get('/todo', function(req, res, next) {
+  res.render('todo');
+});
+router.get('/form', function(req, res, next) {
+  res.render('form');
+});
 
 module.exports = router;
