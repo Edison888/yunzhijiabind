69405989ac3e9050964e0b6dda883ce84e322857
hemingxing <hmx197204@163.com>
2016-11-08 15:59:50 +0800

    审批流图片

---
+++ public/javascripts/flow.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Administrator on 2016/10/26.
+ */
+var app = angular.module('app', []);
+app.controller('flow', function ($scope, $http) {
+    $scope.flowUrl = urlObj.imgUrl;
+});
