53bad46f1220afd1467078eb13891b92f4f3bc78
hemingxing <hmx197204@163.com>
2016-11-08 16:01:27 +0800

    获取审批流图片

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -3,6 +3,31 @@
  */
 var app = angular.module('history', []);
 app.controller('opinion', function ($scope, $http) {
+    $scope.redirectFlow = function (imgUrl) {
+        var uri = new URI('/flow');
+        uri.addQuery('imgUrl', imgUrl);
+        window.location = uri.toString();
+    };
+    $http({
+        method: 'get',
+        url: requrl,
+        params: {
+            billid: urlObj.billid,
+            billtype: urlObj.billtype,
+            taskid: urlObj.taskid,
+            ts: urlObj.ts,
+            method: 'getWorkflow'
+        }
+    }).success(function (response) {
+        console.log(response);
+        $scope.flowUrl = response.data.workflowurl;
+        console.log($scope.flowUrl);
+        //if (response.flag) {
+        //    $scope.historys = response.data.reverse();
+        //} else {
+        //    toastr.error(response.desc);
+        //}
+    });
     $http({
         method: 'get',
         url: requrl,
