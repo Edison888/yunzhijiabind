492c1b5b37a45e5d42a32cd27823ce4ba266b12c
hemingxing <hmx197204@163.com>
2016-11-10 15:39:51 +0800

    显示不同的操作图标。

--- public/javascripts/history.js
+++ public/javascripts/history.js
@@ -2,7 +2,27 @@
  * Created by Administrator on 2016/10/26.
  */
 var app = angular.module('history', []);
-app.controller('opinion', function ($scope, $http) {
+app.filter('selectIcon', function () {
+    return function (input) {
+        var out = '';
+        switch (input) {
+            case '提交':
+                out = 'glyphicon glyphicon-arrow-up item-icon item-icon-blue';
+                break;
+            case '批准':
+                out = 'glyphicon glyphicon-ok-sign item-icon item-icon-green';
+                break;
+            case '不批准':
+                out = 'glyphicon glyphicon-remove-circle item-icon item-icon-red';
+                break;
+            case '驳回':
+                out = 'glyphicon glyphicon-repeat item-icon item-icon-purple';
+                break;
+        }
+
+        return out;
+    }
+}).controller('opinion', function ($scope, $http) {
     $scope.redirectFlow = function (imgUrl) {
         var uri = new URI('/flow');
         uri.addQuery('imgUrl', imgUrl);
@@ -19,14 +39,7 @@ app.controller('opinion', function ($scope, $http) {
             method: 'getWorkflow'
         }
     }).success(function (response) {
-        console.log(response);
         $scope.flowUrl = response.data.workflowurl;
-        console.log($scope.flowUrl);
-        //if (response.flag) {
-        //    $scope.historys = response.data.reverse();
-        //} else {
-        //    toastr.error(response.desc);
-        //}
     });
     $http({
         method: 'get',
