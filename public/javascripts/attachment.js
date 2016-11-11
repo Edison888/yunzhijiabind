5eb0009eda4d32c86b89443f4e064e9c8a5ff3ad
hemingxing <hmx197204@163.com>
2016-11-11 15:40:06 +0800

    显示文件大小。

--- public/javascripts/attachment.js
+++ public/javascripts/attachment.js
@@ -1,7 +1,21 @@
 /**
  * Created by 1 on 2016/11/8.
  */
+function bytesToSize(bytes) {
+    if (bytes === 0) return '0 B';
+
+    var k = 1024;
+
+    sizes = ['B', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+
+    i = Math.floor(Math.log(bytes) / Math.log(k));
+
+    return (bytes / Math.pow(k, i)).toFixed(2) + ' ' + sizes[i];
+}
 angular.module('app', []).controller('attachment', function ($scope, $http) {
+    $scope.showSize = function (size) {
+        return bytesToSize(size);
+    };
     $scope.openFile = function (index) {
         XuntongJSBridge.call('showFile',
             {
