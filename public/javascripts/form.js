9c5a2a5bba3c71cfc7cab54df6f0ddd5e9d49da1
hemingxing <hmx197204@163.com>
2016-11-24 13:58:52 +0800

    修复取消全部会删除人员问题。

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -116,7 +116,6 @@ app.filter('trustHtml', function ($sce) {
         window.location = uri.toString();
     };
     $scope.assigns = [];
-    $scope.allAssigns = [];
     $scope.selecteds = [];
     $scope.selectedUserIdStr = '';
     $scope.isZhiPai = function () {
@@ -127,12 +126,12 @@ app.filter('trustHtml', function ($sce) {
         }
     };
     $scope.selectAll = function () {
-        $scope.selecteds = $scope.allAssigns;
+        $scope.selecteds = ($scope.selecteds).concat($scope.assigns);
         $scope.assigns = [];
     };
     $scope.cancelAll = function () {
+        $scope.assigns = ($scope.selecteds).concat($scope.assigns);
         $scope.selecteds = [];
-        $scope.assigns = $scope.allAssigns;
     };
     $scope.zhipai = function () {
         $scope.selectedUserIdStr = $scope.selecteds[0].id;
@@ -211,11 +210,9 @@ app.filter('trustHtml', function ($sce) {
             console.log(response);
             if (response.flag == 0) {
                 $scope.assigns = [];
-                $scope.allAssigns = [];
                 $scope.selecteds = [];
 
                 if (response.data.isAssign == 'Y') {//有指派信息
-                    $scope.allAssigns = response.data.psnstructlist;
                     $scope.assigns = response.data.psnstructlist;
                     $('#myModal').modal({
                         show: true
