30be6830fd9797d4f405355b40b21ab132ecc6a6
陈浩 <chenhao@uustock.com>
2016-11-24 13:26:28 +0800

    test load data

--- routes/index.js
+++ routes/index.js
@@ -86,7 +86,11 @@ var getUserInfo = function (host, ticket, access_token) {
 var regexAdmin = function (openId) {
     return new Promise(function (resolve, reject) {
         var adminConfig = JSON.parse(fs.readFileSync('./config/admin.json'));
-        console.dir(adminConfig);
+        if (Array.from(adminConfig).find(admin => admin == openId)) {
+            console.log("当前用户为管理员");
+        } else {
+            console.log("当前用户不是管理员");
+        }
         resolve();
     });
 };
