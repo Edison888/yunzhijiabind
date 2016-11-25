4feeafe4a38238d0a3ba551aeae346e1cc29cbaa
陈浩 <chenhao@uustock.com>
2016-11-25 17:21:22 +0800

    qrlogin test

--- routes/index.js
+++ routes/index.js
@@ -29,7 +29,7 @@ router.get('/attachment', function (req, res, next) {
 router.get('/qrcode', function (req, res, next) {
     const sign = uuid.v1();
     console.log(sign);
-    // redisClient.set(sign, false);
+    redisClient.set(sign, null);
     res.render('QRcode', {sign: sign});
     res.end();
 });
