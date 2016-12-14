9b27b3e566605f7ab3789e38e24be12bc4fc81b3
YeasonHe <hmx197204@163.com>
2016-12-14 10:22:00 +0800

    时间戳

--- public/javascripts/attachment.js
+++ public/javascripts/attachment.js
@@ -18,7 +18,7 @@ angular.module('app', []).controller('attachment', function ($scope, $http) {
     };
     $scope.openFile = function (index) {
         XuntongJSBridge.call('setWebViewTitle', {'title': '附件列表'});
-        alert($scope.attachments[index]['name']);
+        alert($scope.attachments[index]['name'].split('.')[0]);
         XuntongJSBridge.call('showFile',
             {
                 fileName: $scope.attachments[index]['name'],
@@ -30,7 +30,6 @@ angular.module('app', []).controller('attachment', function ($scope, $http) {
                 fileDownloadUrl: $scope.attachments[index]['url']
             },
             function (result) {
-                alert(JSON.stringify(result));
                 if (!(result.success)) {
                 }
             }
