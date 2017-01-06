2d541c23da55736325b32515d2a4d6df2d17c57b
陈浩 <chenhao@uustock.com>
2017-01-06 11:03:18 +0800

    add mail-sso import

--- routes/index.js
+++ routes/index.js
@@ -38,6 +38,10 @@ router.get('/seeyon', function (req, res, next) {
     res.render('seeyon-m1');
 });
 
+router.get('/mail/sso', function (req, res, next) {
+    res.render('mail-sso');
+});
+
 router.post('/permission', function (req, res, next) {
     regexAdmin(req.body.openid).then(function (result) {
         res.send(result);
