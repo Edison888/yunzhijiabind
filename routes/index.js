bc129411d10564aefb2d97020a55e4b648bbda46
陈浩 <chenhao@uustock.com>
2017-01-09 00:46:55 +0800

    try to access coremail api at server

--- routes/index.js
+++ routes/index.js
@@ -60,7 +60,7 @@ router.post('/mail/verify/exist', function (req, res, next) {
     }, function (error, status, data) {
         xml2js.parseString(S(data).between('<soap:Body>', '</soap:Body>').s, {trim: true}, function (err, result) {
             var resp = result['ns1:userExistResponse']['return'];
-            console.log(resp[0].code[0]);
+            console.log(resp[0].code[0] == '0');
             res.send({'result': resp.code == '0'});
         });
     });
