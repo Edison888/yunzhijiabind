f6b7e390f85b5d13bc6f16bbd8813133495c40f2
Hao <chenhaoangle@gmail.com>
2018-01-20 20:14:20 +0800

    update

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public'), {
+    maxage: '1d'
+}));
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
 
