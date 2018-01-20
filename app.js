1e79661b9f9a5f856cb5d4242fe6108474112a25
Hao <chenhaoangle@gmail.com>
2018-01-20 22:09:28 +0800

    update

--- app.js
+++ app.js
@@ -29,7 +29,6 @@ app.use(express.static(path.join(__dirname, 'public'), {
 }));
 app.use(helmet());
 app.use(helmet.hidePoweredBy());
-app.use(helmet.frameguard({ action: 'sameorigin' }))
 app.use(helmet.frameguard({
     action: 'allow-from',
     domain: 'http://weibo.gzbfdc.com'
